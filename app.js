02e402a8683cfc15eaebfe946f0840a9fbe2b6f0
j32u4ukh <j32u4ukh@gmail.com>
2023-12-25 21:35:52 +0800

    feat: 建立路由

--- app.js
+++ app.js
@@ -2,36 +2,45 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+const restaurants = require("./public/data/restaurant.json").results;
 
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.redirect("/movies");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/restaurants");
+});
 
-// app.get("/movies", (req, res) => {
-//   const keyword = req.query.search?.trim();
-//   const matchedMovies = keyword
-//     ? movies.filter((mv) =>
-//         Object.values(mv).some((property) => {
-//           if (typeof property === "string") {
-//             return property.toLowerCase().includes(keyword.toLowerCase());
-//           }
-//           return false;
-//         })
-//       )
-//     : movies;
-//   res.render("index", { movies: matchedMovies, BASE_IMG_URL, keyword });
-// });
+app.get("/restaurants", (req, res) => {
+  res.render("index", { restaurants: restaurants, keyword: "" });
+});
 
-// app.get("/movie/:id", (req, res) => {
-//   const id = Number(req.params.id);
-//   const movie = movies.find((m) => m.id === id);
-//   res.render("detail", { movie, BASE_IMG_URL });
-// });
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword?.trim();
+  let results;
+  if (keyword === "") {
+    results = restaurants;
+  } else {
+    const key = keyword.toLowerCase();
+    results = results.filter((result) =>
+      Object.values(result).some((r) => {
+        if (typeof r === "string") {
+          return r.toLowerCase().includes(key);
+        }
+        return false;
+      })
+    );
+  }
+  res.render("index", { restaurants: results, keyword: keyword });
+});
+
+app.get("/restaurant/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const restaurant = restaurants.find((m) => m.id === id);
+  res.render("show", { restaurant });
+});
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`);

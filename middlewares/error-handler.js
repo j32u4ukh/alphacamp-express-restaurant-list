962ece7394f310d558dea8089f2bb9c3f7259411
j32u4ukh <j32u4ukh@gmail.com>
2024-01-28 22:29:18 +0800

    refactor: 調整版面配置

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,6 +1,6 @@
 module.exports = (error, req, res, next) => {
   console.error(error);
-  console.log("Referer:", req.get("Referer"));
+  // console.log("Referer:", req.get("Referer"));
   req.flash("error", error.errorMessage || "處理失敗:(");
   if (error.redirect) {
     res.redirect(error.redirect);
